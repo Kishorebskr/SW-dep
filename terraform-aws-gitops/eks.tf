05be50ffda3431650c7838a20ad629ca19586b35
Kishore <kishore97bala@gmail.com>
2025-09-24 21:25:56 +0530

    ini

--- terraform-aws-gitops/eks.tf
+++ terraform-aws-gitops/eks.tf
@@ -16,8 +16,9 @@ module "eks" {
   cluster_endpoint_private_access = true
 
   # Disable default KMS key and CloudWatch log group creation
-  create_kms_key               = false
-  create_cloudwatch_log_group  = false
+  create_kms_key              = false
+  create_cloudwatch_log_group = false
+  cluster_encryption_config    = {}
 
   eks_managed_node_groups = {
     default = {
