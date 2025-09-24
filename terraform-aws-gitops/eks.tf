161c9d04ffaacf19142b0b3f6010ee9ad3c3b2c7
Kishore <kishore97bala@gmail.com>
2025-09-24 20:27:40 +0530

    ini

--- terraform-aws-gitops/eks.tf
+++ terraform-aws-gitops/eks.tf
@@ -11,14 +11,15 @@ module "eks" {
   vpc_id          = module.vpc.vpc_id
   subnet_ids      = module.vpc.private_subnets
 
-  manage_aws_auth = true
-
-  node_groups = {
+  # v19+ uses eks_managed_node_groups instead of node_groups
+  eks_managed_node_groups = {
     default = {
-      desired_capacity = 2
-      min_capacity     = 1
-      max_capacity     = 3
-      instance_types   = ["t3.medium"]
+      min_size     = 1
+      max_size     = 3
+      desired_size = 2
+
+      instance_types = ["t3.medium"]
+      capacity_type  = "ON_DEMAND"
     }
   }
 
