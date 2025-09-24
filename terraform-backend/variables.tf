a79b64719ccc04bb6fb6bc611cba15f704f1dc0d
Kishore <kishore97bala@gmail.com>
2025-09-24 19:49:36 +0530

    ini

--- terraform-backend/variables.tf
+++ terraform-backend/variables.tf
@@ -1,3 +1,17 @@
-variable "aws_region" { default = "eu-central-1" }
-variable "s3_bucket_name" { default = "sw-terraform-states" }
-variable "dynamodb_table_name" { default = "sw-terraform-locks" }
+variable "aws_region" {
+  description = "AWS region for backend"
+  type        = string
+  default     = "eu-central-1"
+}
+
+variable "s3_bucket_name" {
+  description = "Base name of the S3 bucket for Terraform state (suffix added automatically)"
+  type        = string
+  default     = "sw-terraform-states"
+}
+
+variable "dynamodb_table_name" {
+  description = "Name of the DynamoDB table for Terraform state locks"
+  type        = string
+  default     = "sw-terraform-locks"
+}
